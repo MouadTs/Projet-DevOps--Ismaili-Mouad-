b39a2b872a82a6541c91299ceaf8273097d02d18
mouadTs <ionia456@outlook.fr>
2026-01-08 13:19:22 +0100

    jenkins file commit second

--- App.java
+++ App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Bonjour et bon courage dans votre projet en DevOps");
         System.out.println("Deuxieme ligne pour le test de commit... ");
+        System.out.println("Test Jenkins auto build on push");
+
     }
 }
